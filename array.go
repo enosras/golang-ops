42fd058e8ad669cd89f1efa9558436a06bd2fca9
enos <enyamor@gmail.com>
2025-11-11 08:44:38 -0800

    arrays tutorials

---
+++ array.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//fmt.Println("Enter the size of array please : ")
+	//var size_arr int = 3
+	//fmt.Scanf("%d", &size_arr)
+
+	var arr [3]string = [3]string{"Enoh", "Amor", "Otis"}
+	fmt.Println(arr[2])
+	fmt.Println("Wait for it: ")
+	//var length int = len(arr[])
+	//fmt.Println(len(arr))
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(arr[i])
+
+	}
+
+}
